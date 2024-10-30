8082cf1a2aeb3bb30da9f48b1f3670b819595ebe
joycetan913 <joycetanyy913@gmail.com>
2024-10-30 16:27:52 +0800

    initial commit

--- vttp_b5_assessment_template/task02/src/vttp/batch5/sdf/task02/Main.java
+++ vttp_b5_assessment_template/task02/src/vttp/batch5/sdf/task02/Main.java
@@ -3,6 +3,7 @@ package vttp.batch5.sdf.task02;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 	
@@ -13,32 +14,7 @@ public class Main {
 		if (args.length < 0)
 			System.err.println("No TTT configuration file provided.");
 
-		try(BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
-			String line;
-			while ((line = br.readLine()) != null)
-				String[] values = line.split("");
-
-		} catch (IOException e) {
-			System.out.println("Error reading file:" + e.getMessage());
-		}
-
-		char [][]board = new char[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '.';
-            }
-        }
-        currentPlayer = 'X'; // X starts first
-    
-
-    	public void printBoard() {
-        	for (int i = 0; i < 3; i++) {
-             	System.out.print(board[i][0] + board[i][1] + board[i][2]);
-            	if (i < 2)
-                	System.out.println("\n-----------");
-			}
-    	}
-    
-	
+		TicTacToe game = new TicTacToe();
 	}
+    
 }
--- vttp_b5_assessment_template/task02/src/vttp/batch5/sdf/task02/TicTacToe.java
+++ vttp_b5_assessment_template/task02/src/vttp/batch5/sdf/task02/TicTacToe.java
@@ -1,47 +1,59 @@
 package vttp.batch5.sdf.task02;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
 public class TicTacToe {
 
     private String inputFile;
     private char[][] board = null;
+    private int width = 0;
+    private int height = 0;
     private char currentPlayer;
     
-    public TicTacToe(String ttt) {
-        inputFile = ttt;
-
     public TicTacToe(char [][] b){
         board = b;
-        height = b.length;
-        width = b[0].length;
+        b = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '';
+            }
+        currentPlayer = 'X'; 
+        }
+    }
+
+	public void printBoard() {
+		for (int i = 0; i < 3; i++) {
+			System.out.print(" " + board[i][0] + board[i][1] + board[i][2] + " ");
+			if (i < 2)
+				System.out.println("\n");
+			}
+		}
+
+	public String getUtility(){
+		for (int i = 0; i < 3; i++) {
+			if ((board[0][0] == player)) {
+				return 0;
+			} else if ((board[1][0] == player || board[1][2] == player )) {
+				return -1;
+		    } else if ((board[2][2]) == player)) {
+				return 1;
+			}
+        }   
     }
 
     public void readFile() throws Exception {
 
-        Reader reader = new FileReader(inputFile);
-        BufferedReader br = new BufferedReader(reader);
-        String line;
-  
-        while ((line = br.readLine()) != null) {
-           line = line.trim();
-        }
+        try(BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+			String line;
+			while ((line = br.readLine()) != null)
+				String[] values = line.trim();
 
-        String terms[] = line.split(" ");
-        if (Constants.GRID.equals(terms[0])) {
-           width = Integer.parseInt(terms[1]);
-           height = Integer.parseInt(terms[2]);
-           board = new char[height][0];
-
-   
-    
-    public boolean isBoardFull() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == ' ') {
-                    return false;
-                }
-            }
-        }
-        return true;
+		} catch (IOException e) {
+			System.out.println("Error reading file:" + e.getMessage());
+		}
     }
 
 }
